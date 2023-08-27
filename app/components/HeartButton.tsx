742ff7f06ca79cf87a6d0606f879c856e699c207
solssak <gswoong1012@gmail.com>
2023-08-27 22:44:51 +0900

    feat: 장소목록 좋아요 기능 구현

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavortie from "../hooks/useFavorite";
 
 interface HeartButtonProps {
   listingId: string;
@@ -12,8 +13,10 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
-  const hasFavorited = false;
-  const toggleFavorite = () => {};
+  const { hasFavorited, toggleFavorite } = useFavortie({
+    listingId,
+    currentUser,
+  });
 
   return (
     <div
