c8fde15b12aade8f860dc59f7d727eba6aaae8a3
solssak <gswoong1012@gmail.com>
2023-08-28 11:46:33 +0900

    feat: 장소 상세페이지 기본 정보 목록 구현

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -6,13 +6,13 @@ import { Listing, Reservation } from "@prisma/client";
 import { format } from "date-fns";
 
 import useCountries from "@/app/hooks/useCountries";
-import { SafeUser } from "@/app/types";
+import { SafeListing, SafeUser } from "@/app/types";
 import { useCallback, useMemo } from "react";
 
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,9 @@
-import { User } from "@prisma/client";
+import { Listing, User } from "@prisma/client";
+
+// 날짜와 시간 정보가 아닌 문자열 형태로 저장하기 위함
+export type SafeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string;
+};
 
 export type SafeUser = Omit<
   User,
