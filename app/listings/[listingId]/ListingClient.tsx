c47a6152e00973283b62e500906264e66715f54a
solssak <gswoong1012@gmail.com>
2023-09-02 23:58:34 +0900

    test: vercel 배포 오류 테스트

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import { differenceInCalendarDays, eachDayOfInterval } from "date-fns";
 import { useCallback, useEffect, useMemo, useState } from "react";
